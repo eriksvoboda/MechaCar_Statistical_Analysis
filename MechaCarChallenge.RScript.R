205482a2f9ddb81af1cc9581d9340dc2660d52b7
eriksvoboda <93800235+eriksvoboda@users.noreply.github.com>
2022-04-12 16:08:05 -0500

    Add files via upload
    Adding finished R file

---
+++ MechaCarChallenge.RScript.R
@@ -0,0 +1,19 @@
+library(dplyr)
+mech_df <- read.csv(file = 'MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)
+head(mech_df)
+reg <- lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data = mech_df)
+summary(reg)
+
+sus_df <- read.csv(file = 'Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+head(sus_df)
+# create total summary for PSI
+total_summary <- sus_df %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+# create lot summary 
+lot_summary <- sus_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+
+# t tests
+t.test(sus_df$PSI, mu=1500)
+
+t.test(subset(sus_df,Manufacturing_Lot=="Lot1")$PSI, mu = 1500)
+t.test(subset(sus_df,Manufacturing_Lot=="Lot2")$PSI, mu = 1500)
+t.test(subset(sus_df,Manufacturing_Lot=="Lot3")$PSI, mu = 1500)
